c46f4af9d63e63273420c242483acb8c7a809daa
Debashis Biswas <debashis.biswas@asu.edu>
2019-07-25 15:05:24 -0700

    fixed carousel swipe on mobile

--- scripts/main.js
+++ scripts/main.js
@@ -32,7 +32,8 @@ $( '#begin-button' ).on( 'click' , (function() {
         randomizedExercises = shuffle( randomizedExercises );    
         $( '#carousel-output-container' ).append( generateKeyHTML( currentKeyCount, randomizedKeys.length, theKey, randomizedExercises ) );
     }
-
+    
+    $( '#main-carousel' ).carousel();
     hideCarouselEnds();
 }));
 
